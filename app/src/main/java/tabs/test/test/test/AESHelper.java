7b642ed7c2e2d5a2e454e9673b92af2e985d93dc
Spike <spike@inernetbs.net>
2018-04-12 12:31:57 +0300

    6.4.2 interstitial work

---
+++ app/src/main/java/tabs/test/test/test/AESHelper.java
@@ -0,0 +1,124 @@
+package tabs.test.test.test;
+
+import java.security.Provider;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AESHelper {
+    public static final String KEY = "CB57A0E06398C7DD8E676EFB1D6476F3D9E8941BBEB3F7455087068BD1868FBF14D3460C089B9C7C194A678F90823841D9E545DF66E983E3D0CF62ECB6A1FFBEAE7B7A4A53B18628242857A2486424B2AD2DB2BB4E97CEEE30158C081FC2CFD6";
+    private final static String HEX = "0123456789ABCDEF";
+
+    public static String encryption(String strNormalText){
+        String seedValue = "nbfdnb667yhFGKjk1";
+        String normalTextEnc="";
+        try {
+            normalTextEnc = AESHelper.encrypt(seedValue, strNormalText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return normalTextEnc;
+    }
+    public static String decryption(String strEncryptedText){
+        String seedValue = "nbfdnb667yhFGKjk1";
+        String strDecryptedText="";
+        try {
+            strDecryptedText = AESHelper.decrypt(seedValue, strEncryptedText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return strDecryptedText;
+    }
+
+    private static String encrypt(String seed, String cleartext) throws Exception {
+        byte[] rawKey = getRawKey(seed.getBytes());
+        byte[] result = encrypt(rawKey, cleartext.getBytes());
+        return toHex(result);
+    }
+
+    private static String decrypt(String seed, String encrypted) throws Exception {
+        byte[] rawKey = getRawKey(seed.getBytes());
+        byte[] enc = toByte(encrypted);
+        byte[] result = decrypt(rawKey, enc);
+        return new String(result);
+    }
+
+
+    private static String toHex(String txt) {
+        return toHex(txt.getBytes());
+    }
+
+    private static String fromHex(String hex) {
+        return new String(toByte(hex));
+    }
+
+    private static byte[] toByte(String hexString) {
+        int len = hexString.length() / 2;
+        byte[] result = new byte[len];
+        for (int i = 0; i < len; i++)
+            result[i] = Integer.valueOf(hexString.substring(2 * i, 2 * i + 2), 16).byteValue();
+        return result;
+    }
+
+    private static String toHex(byte[] buf) {
+        if (buf == null)
+            return "";
+        StringBuffer result = new StringBuffer(2 * buf.length);
+        for (int i = 0; i < buf.length; i++) {
+            appendHex(result, buf[i]);
+        }
+        return result.toString();
+    }
+
+    private static byte[] getRawKey(byte[] seed) throws Exception {
+        KeyGenerator kgen = KeyGenerator.getInstance("AES");
+        SecureRandom sr;
+        try {
+            sr = SecureRandom.getInstance("SHA1PRNG", "Crypto");
+        } catch (Exception e) {
+            e.printStackTrace();
+            sr = SecureRandom.getInstance("SHA1PRNG", new CryptoProvider());
+        }
+        sr.setSeed(seed);
+        kgen.init(128, sr); // 192 and 256 bits may not be available
+        SecretKey skey = kgen.generateKey();
+        byte[] raw = skey.getEncoded();
+        return raw;
+    }
+
+    private static byte[] encrypt(byte[] raw, byte[] clear) throws Exception {
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+        byte[] encrypted = cipher.doFinal(clear);
+        return encrypted;
+    }
+
+    private static byte[] decrypt(byte[] raw, byte[] encrypted) throws Exception {
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+        byte[] decrypted = cipher.doFinal(encrypted);
+        return decrypted;
+    }
+
+    private static void appendHex(StringBuffer sb, byte b) {
+        sb.append(HEX.charAt((b >> 4) & 0x0f)).append(HEX.charAt(b & 0x0f));
+
+    }
+
+    private static class CryptoProvider extends Provider {
+        /**
+         * Creates a Provider and puts parameters
+         */
+        CryptoProvider() {
+            super("Crypto", 1.0, "HARMONY (SHA1 digest; SecureRandom; SHA1withDSA signature)");
+            put("SecureRandom.SHA1PRNG",
+                    "org.apache.harmony.security.provider.crypto.SHA1PRNG_SecureRandomImpl");
+            put("SecureRandom.SHA1PRNG ImplementedIn", "Software");
+        }
+    }
+}
--- app/src/main/java/tabs/test/test/test/MainActivity.java
+++ app/src/main/java/tabs/test/test/test/MainActivity.java
@@ -2,73 +2,165 @@ package tabs.test.test.test;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.applovin.adview.AppLovinIncentivizedInterstitial;
+import com.applovin.adview.AppLovinInterstitialAd;
+import com.applovin.adview.AppLovinInterstitialAdDialog;
+import com.applovin.sdk.AppLovinAd;
+import com.applovin.sdk.AppLovinAdDisplayListener;
+import com.applovin.sdk.AppLovinAdLoadListener;
+import com.applovin.sdk.AppLovinAdRewardListener;
+import com.applovin.sdk.AppLovinAdSize;
+import com.applovin.sdk.AppLovinErrorCodes;
+import com.applovin.sdk.AppLovinSdk;
+import com.applovin.sdk.AppLovinSdkSettings;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnShowInterstitial;
 
-    List<Person> persons;
-    RecyclerView recyclerView;
-    RVAdapter rvAdapter;
-    MyLayoutManager layoutManager;
+    /*       APPLOVIN     */
+    AppLovinSdk appLovinSdk;
+    AppLovinAdLoadListener mAdLoadListener;
+    AppLovinAdRewardListener mAdRewardListener;
+    AppLovinAdDisplayListener mAdDisplayListener;
+    AppLovinIncentivizedInterstitial myIncent;
+    public AppLovinAd appLovinAd = null;
+    AppLovinInterstitialAdDialog appLovinAdDialog;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initializeList();
-        recyclerView = (RecyclerView)findViewById(R.id.rv);
-        rvAdapter = new RVAdapter(persons);
-        System.out.println(persons.size());
 
-        layoutManager =new MyLayoutManager(this);
-        layoutManager.setAutoMeasureEnabled(true);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(rvAdapter);
+        btnShowInterstitial = (Button) findViewById(R.id.btnShowInterstitial);
+
+        btnShowInterstitial.setOnClickListener(v -> {
+            showInterstitial();
+        });
+
+        initializeApplovin();
 
     }
 
-    private void initializeList(){
-        persons = new ArrayList<>();
-        persons.add(new Person("Emma Wilson", "23 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lavery Maiss", "25 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Watts", "35 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Wilson", "44 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Watts", "35 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Wilson Watts", "35 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Watts", "33 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Maiss Watts", "66 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Watts Lavery", "77 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Wilson Watts", "35 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Watts", "11 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Wilson Watts", "33 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Emma Wilson", "23 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lavery Maiss", "25 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Watts", "35 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Wilson", "44 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Watts", "35 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Wilson Watts", "35 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Watts", "33 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Maiss Watts", "66 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Watts Lavery", "77 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Wilson Watts", "35 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Watts", "11 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Wilson Watts", "33 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Emma Wilson", "23 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lavery Maiss", "25 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Watts", "35 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Wilson", "44 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Watts", "35 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Wilson Watts", "35 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Watts", "33 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Maiss Watts", "66 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Watts Lavery", "77 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Wilson Watts", "35 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Lillie Watts", "11 years old", R.mipmap.ic_launcher));
-        persons.add(new Person("Wilson Watts", "33 years old", R.mipmap.ic_launcher));
+    private void initializeApplovin() {
+        initListeners();
+        AppLovinSdkSettings appLovinSdkSettings = new AppLovinSdkSettings();
+        appLovinSdkSettings.setTestAdsEnabled(true);
+        appLovinSdkSettings.setVerboseLogging(true);
+
+        appLovinSdk = AppLovinSdk.getInstance(AESHelper.decryption(AESHelper.KEY), appLovinSdkSettings, this);
+        appLovinSdk.initializeSdk();
+
+        myIncent = AppLovinIncentivizedInterstitial.create(appLovinSdk);
+        myIncent.preload(mAdLoadListener);
+
+        appLovinAdDialog = AppLovinInterstitialAd.create(appLovinSdk, this);
+
+        //   loadInterstitial();
+
     }
+
+    private void initListeners() {
+        mAdLoadListener = new AppLovinAdLoadListener() {
+            @Override
+            public void adReceived(AppLovinAd ad) {
+                appLovinAd = ad;
+                Log.d("AppLovinDebug", "adReceived: ");
+                Toast.makeText(getApplicationContext(), "loadin OK !", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void failedToReceiveAd(int i) {
+                Log.d("AppLovinDebug", "failedToReceiveAd: " + i);
+                Toast.makeText(getApplicationContext(), "loadin FAILED!" + i, Toast.LENGTH_LONG).show();
+                new Handler().postDelayed(() -> myIncent.preload(mAdLoadListener), 5000);
+            }
+        };
+
+        mAdRewardListener = new AppLovinAdRewardListener() {
+
+
+            @Override
+            public void userRewardVerified(AppLovinAd appLovinAd, Map map) {
+
+            }
+
+            @Override
+            public void userOverQuota(AppLovinAd appLovinAd, Map map) {
+
+            }
+
+            @Override
+            public void userRewardRejected(AppLovinAd appLovinAd, Map map) {
+
+            }
+
+            @Override
+            public void validationRequestFailed(AppLovinAd appLovinAd, int i) {
+
+            }
+
+            @Override
+            public void userDeclinedToViewAd(AppLovinAd appLovinAd) {
+
+            }
+        };
+        mAdDisplayListener = new AppLovinAdDisplayListener() {
+            @Override
+            public void adDisplayed(AppLovinAd appLovinAd) {
+                Log.d("AppLovinDebug", "adDisplayed: ");
+            }
+
+            @Override
+            public void adHidden(AppLovinAd appLovinAd) {
+
+
+            }
+        };
+
+    }
+
+//    private void showVideo(){
+//        myIncent.show(this, mAdRewardListener, null, mAdDisplayListener);
+//    }
+//
+    private void showInterstitial(){
+        appLovinAdDialog.setAdDisplayListener(new AppLovinAdDisplayListener() {
+            @Override
+            public void adDisplayed(AppLovinAd appLovinAd) {
+
+            }
+
+            @Override
+            public void adHidden(AppLovinAd appLovinAd) {
+
+            }
+        });
+        appLovinAdDialog.showAndRender(appLovinAd);
+    }
+//    private void loadInterstitial(){
+//        appLovinSdk.getAdService().loadNextAd(AppLovinAdSize.INTERSTITIAL, new AppLovinAdLoadListener() {
+//            @Override
+//            public void adReceived(AppLovinAd ad) {
+//               appLovinAd = ad;
+//            }
+//
+//            @Override
+//            public void failedToReceiveAd(int i) {
+//                Log.d("AppLovinDebug", "failed load interstitial: " + i);
+//            }
+//        });
+//
+//    }
 }
